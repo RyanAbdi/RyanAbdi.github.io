c1cb88f088c66c8a551dd256df36ddaec2d81b64
Ryan Abdi <80437941+RyanAbdi@users.noreply.github.com>
2025-09-01 20:56:01 -0400

    Fix Image again?

--- portfolio-website/next.config.mjs
+++ portfolio-website/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = "RyanAbdi.github.io";
 const nextConfig = {
     output: "export",
+    basePath: "",
+    assetPrefix: "/" + repoName + "/",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
